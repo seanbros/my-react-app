34dfbbfb7b5533a4a766feeaddc7071330c6b5c5
Sean Brosnahan <b.sean@wustl.edu>
2025-04-01 16:48:48 -0500

    initial error code when copy and pasting code 9 times

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,35 @@ import { useState } from 'react';
 export default function Square() {
   return <button className="square">X</button>;
 }
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
+
+export default function Square() {
+  return <button className="square">X</button>;
+}
