55323665680bc2659cad313f246a89c885e3cac7
Sean Brosnahan <b.sean@wustl.edu>
2025-04-01 16:49:29 -0500

    fixed code using fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
 import { useState } from 'react';
 
 export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
-}
-
-export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
